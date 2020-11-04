1cc0b1838032d964a5860a758d4daf5ea5162a21
brian chiem <brianfamily5@gmail.com>
2020-11-04 08:46:42 -0700

    Add Data for appointments and import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Appointment from './Components/Appointment';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>Appointment List</h1>
+        <Appointment />
       </div>
     )
   }
--- src/Components/Appointment.js
+++ src/Components/Appointment.js
@@ -1,21 +1,10 @@
 import React, { Component } from 'react'
 
 export default class Appointment extends Component {
-    state = {
-            appointments: [
-            {
-            Customer: { firstname: 'Ashley', lastName: 'Jones', phoneNumber: '(780) 123-1234' },
-            stylist: 'Kobe Bryant',
-            startsAt: '2019-02-20 09:30',
-            service: 'cut',
-            notes: ''
-            },
-        ]
-    }
     render() {
         return (
             <div>
-                <p>{this.state.appointments}</p>
+                <p>hello</p>
             </div>
         )
     }
