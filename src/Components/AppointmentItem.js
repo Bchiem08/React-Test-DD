a3dd2992146581afc27cb7248206babd9e1470af
brian chiem <brianfamily5@gmail.com>
2020-11-04 09:19:38 -0700

    Add appointment info to page

--- src/Components/AppointmentItem.js
+++ src/Components/AppointmentItem.js
@@ -4,7 +4,13 @@ export default class AppointmentItem extends Component {
     render() {
         return (
             <div>
+                <p>{this.props.AppointmentInfo.customer.firstName}</p>
+                <p>{this.props.AppointmentInfo.customer.lastName}</p>
+                <p>{this.props.AppointmentInfo.customer.phoneNumber}</p>
                 <p>{this.props.AppointmentInfo.stylist}</p>
+                <p>{this.props.AppointmentInfo.startsAt}</p>
+                <p>{this.props.AppointmentInfo.service}</p>
+                <p>{this.props.AppointmentInfo.notes}</p>
             </div>
         )
     }
